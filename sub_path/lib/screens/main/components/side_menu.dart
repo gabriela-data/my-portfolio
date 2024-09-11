e80d97b8111a6e31bcee927a658af1f0469a73cd
Gabriela Almeida <gabiecanelaa@gmail.com>
2024-09-11 18:50:55 -0300

    Primeiro commit

---
+++ sub_path/lib/screens/main/components/side_menu.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_profile/constants.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+import 'area_info_text.dart';
+import 'coding.dart';
+import 'knowledges.dart';
+import 'my_info.dart';
+import 'skills.dart';
+
+class SideMenu extends StatelessWidget {
+  const SideMenu({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SafeArea(
+        child: Column(
+          children: [
+            MyInfo(),
+            Expanded(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Column(
+                  children: [
+                    AreaInfoText(
+                      title: "País",
+                      text: "Brasil",
+                    ),
+                    AreaInfoText(
+                      title: "Cidade",
+                      text: "Salvador",
+                    ),
+                    AreaInfoText(
+                      title: "Idade",
+                      text: "28",
+                    ),
+                    Skills(),
+                    SizedBox(height: defaultPadding),
+                    Coding(),
+                    Knowledges(),
+                    Divider(),
+                    SizedBox(height: defaultPadding / 2),
+                    TextButton(
+                      onPressed: () {},
+                      child: FittedBox(
+                        child: Row(
+                          children: [
+                            Text(
+                              "DOWNLOAD CV",
+                              style: TextStyle(
+                                color: Theme.of(context)
+                                    .textTheme
+                                    .bodyText1!
+                                    .color,
+                              ),
+                            ),
+                            SizedBox(width: defaultPadding / 2),
+                            SvgPicture.asset("assets/icons/download.svg")
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(top: defaultPadding),
+                      color: Color(0xFF24242E),
+                      child: Row(
+                        children: [
+                          Spacer(),
+                          IconButton(
+                            onPressed: () async {
+                              const url =
+                                  'https://www.linkedin.com/in/gabriela-ssa/';
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                            icon: SvgPicture.asset("assets/icons/linkedin.svg"),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              const url =
+                                  'https://github.com/gabriela-data';
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                            icon: SvgPicture.asset("assets/icons/github.svg"),
+                          ),
+                          IconButton(
+                            onPressed: () async {
+                              const url =
+                                  '';
+                              if (await canLaunch(url)) {
+                                await launch(url);
+                              } else {
+                                throw 'Could not launch $url';
+                              }
+                            },
+                            icon: SvgPicture.asset("assets/icons/medium.svg"),
+                          ),
+                          Spacer(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
